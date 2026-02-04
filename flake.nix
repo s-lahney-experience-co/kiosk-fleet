039698a18cec5265184d186391ea1f5bc976b00a
s-lahney-experience-co <s.lahney@experienceco.com>
2026-02-05 09:13:11 +1000

    Update flake.nix
    changed host names

--- flake.nix
+++ flake.nix
@@ -8,38 +8,280 @@
   outputs = { self, nixpkgs }: {
     nixosConfigurations = {
       # Example kiosk configurations - add more as needed
-      kiosk-01 = nixpkgs.lib.nixosSystem {
+      exp-stream-001 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
           ./kiosk-common.nix
           {
-            networking.hostName = "kiosk-01";
+            networking.hostName = "exp-stream-001";
           }
         ];
       };
 
-      kiosk-02 = nixpkgs.lib.nixosSystem {
+      exp-stream-002 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
           ./kiosk-common.nix
           {
-            networking.hostName = "kiosk-02";
+            networking.hostName = "exp-stream-002";
           }
         ];
       };
 
-      kiosk-03 = nixpkgs.lib.nixosSystem {
+      exp-stream-003 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hardware-configuration.nix
           ./kiosk-common.nix
           {
-            networking.hostName = "kiosk-03";
+            networking.hostName = "exp-stream-003";
           }
         ];
       };
+	  
+      exp-stream-004 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-004";
+          }
+        ];
+      };
+
+      exp-stream-005 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-005";
+          }
+        ];
+      };
+
+      exp-stream-006 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-006";
+          }
+        ];
+      };	  
+	  
+      exp-stream-007 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-007";
+          }
+        ];
+      };
+
+      exp-stream-008 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-008";
+          }
+        ];
+      };
+
+      exp-stream-009 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-009";
+          }
+        ];
+      };
+	  
+      exp-stream-010 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-010";
+          }
+        ];
+      };
+
+      exp-stream-011 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-011";
+          }
+        ];
+      };
+
+      exp-stream-012 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-012";
+          }
+        ];
+      };	  
+	  
+      exp-stream-013 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-013";
+          }
+        ];
+      };
+
+      exp-stream-014 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-014";
+          }
+        ];
+      };
+
+      exp-stream-015 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-015";
+          }
+        ];
+      };
+	  
+      exp-stream-016 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-016";
+          }
+        ];
+      };
+
+      exp-stream-017 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-017";
+          }
+        ];
+      };
+
+      exp-stream-018 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-018";
+          }
+        ];
+      };	  
+	  
+      exp-stream-019 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-019";
+          }
+        ];
+      };
+
+      exp-stream-020 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-020";
+          }
+        ];
+      };
+
+      exp-stream-021 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-021";
+          }
+        ];
+      };
+	  
+      exp-stream-022 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-022";
+          }
+        ];
+      };
+
+      exp-stream-023 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-023";
+          }
+        ];
+      };
+
+      exp-stream-024 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-024";
+          }
+        ];
+      };
+
+      exp-stream-025 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hardware-configuration.nix
+          ./kiosk-common.nix
+          {
+            networking.hostName = "exp-stream-025";
+          }
+        ];
+      };	  
     };
   };
 }
