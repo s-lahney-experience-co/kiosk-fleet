43f7490d4590288d4eebb0eba82b4de47c52ca7c
s-lahney-experience-co <s.lahney@experienceco.com>
2026-02-04 11:45:41 +1000

    Update flake.nix
    updated to nixos-unstable to get more recent version of google-chrome

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "ExperienceCo Kiosk Fleet Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
