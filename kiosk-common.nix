567b598841e7a424b02038033fe85feff030c342
s-lahney-experience-co <s.lahney@experienceco.com>
2026-02-06 11:35:09 +1000

    Update kiosk-common.nix
    added  import of hardware configuration

--- kiosk-common.nix
+++ kiosk-common.nix
@@ -1,16 +1,25 @@
 { config, pkgs, ... }:
 {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
   # Allow unfree packages like google-chrome
   nixpkgs.config.allowUnfree = true;
+  
   # System state version - don't change this after installation
   system.stateVersion = "24.05";
+  
   # Boot loader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  
   # Enable networking
   networking.networkmanager.enable = true;
+  
   # Time zone
   time.timeZone = "Australia/Sydney";
+  
   # Automatic updates from your flake repository
   system.autoUpgrade = {
     enable = true;
@@ -22,21 +31,28 @@
       "--commit-lock-file"
     ];
   };
+  
   # Automatic garbage collection to save disk space
   nix.gc = {
     automatic = true;
-    dates = "daily";  # Daily for microSD cards (weekly for larger storage)
-    options = "--delete-older-than 30d";
+    dates = "daily";
   };
+  
+  # Keep only the last 5 system generations
+  boot.loader.systemd-boot.configurationLimit = 5;  # Adjust based on your bootloader
+  
   # Limit number of boot generations (important for microSD cards)
   boot.loader.systemd-boot.configurationLimit = 5;
+  
   # Limit journal size to save space
   services.journald.extraConfig = ''
     SystemMaxUse=200M
     SystemMaxFileSize=50M
   '';
+  
   # Enable flakes
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
   # Install required packages
   environment.systemPackages = with pkgs; [
     google-chrome
@@ -47,6 +63,7 @@
     unclutter
     xorg.xset
   ];
+  
   # X11 windowing system
   services.xserver = {
     enable = true;
@@ -55,11 +72,13 @@
     windowManager.openbox.enable = true;
     displayManager.lightdm.enable = true;
   };
+  
   # Auto-login for kiosk user (must be outside services.xserver in newer NixOS)
   services.displayManager.autoLogin = {
     enable = true;
     user = "kiosk";
   };
+  
   # Disable screen blanking and power management
   services.xserver.serverFlagsSection = ''
     Option "BlankTime" "0"
@@ -67,18 +86,21 @@
     Option "SuspendTime" "0"
     Option "OffTime" "0"
   '';
+  
   # Create kiosk user
   users.users.kiosk = {
     isNormalUser = true;
     description = "Kiosk User";
     extraGroups = [ "networkmanager" "video" "audio" "camera" ];
   };
+  
   # Allow camera access
   services.udev.extraRules = ''
     # Allow video group to access webcams
     SUBSYSTEM=="video4linux", GROUP="video", MODE="0660"
     SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="*", ATTRS{idProduct}=="*", MODE="0660", GROUP="video"
   '';
+  
   # Create .xprofile to launch Google Chrome in kiosk mode
   system.activationScripts.kioskXprofile = ''
     mkdir -p /home/kiosk
